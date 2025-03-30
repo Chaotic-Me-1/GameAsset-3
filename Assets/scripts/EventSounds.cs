6976b579fc960a47549d0941e5e987505f4f42e5
OlafRT <mcpikarar@gmail.com>
2025-03-31 00:34:48 +0200

    oads update

---
+++ Assets/scripts/EventSounds.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class EventSounds : MonoBehaviour
+{
+    [Header("FMOD Events")]
+    [SerializeField] private EventReference stepSound;
+    [SerializeField] private EventReference talkSound;
+    [SerializeField] private EventReference serveSound;
+
+    //Playing sounds with Animation Events, we set these up in the animation that will trigger the sound. 
+    //For example "Step" would be triggered by the animation event "Step" whenever the flight attendant takes a step.
+
+    public void Step() //footstep
+    {
+        PlaySound(stepSound);
+    }
+
+    public void Talk() //talking
+    {
+        PlaySound(talkSound);
+    }
+
+    public void Serve() //serving food
+    {
+        PlaySound(serveSound);
+    }
+
+    private void PlaySound(EventReference soundEvent) //plays the sound that is meant to be playing at the transform position of the gameobject with this script on it.
+    {
+        if (soundEvent.IsNull) return; //if there isnt a fmod sound event assigned we skip this so we don't get a gazillion errors.
+
+        RuntimeManager.PlayOneShot(soundEvent, transform.position);
+    }
+}
