50e47da2c9c7d7b0b015fb3ed3795512a65a1fa4
OdinIMG2 <Odinhegdahl@hotmail.com>
2025-03-31 01:59:59 +0200

    Testing / tweeking  event sound

--- Assets/scripts/EventSounds.cs
+++ Assets/scripts/EventSounds.cs
@@ -8,29 +8,50 @@ public class EventSounds : MonoBehaviour
     [SerializeField] private EventReference stepSound;
     [SerializeField] private EventReference talkSound;
     [SerializeField] private EventReference serveSound;
+    [SerializeField] private Animator attendent;
+
 
     //Playing sounds with Animation Events, we set these up in the animation that will trigger the sound. 
     //For example "Step" would be triggered by the animation event "Step" whenever the flight attendant takes a step.
 
+    
     public void Step() //footstep
     {
-        PlaySound(stepSound);
+        
+        if (attendent.GetBool("IsPushing") == true)
+        {
+            PlaySound(stepSound);
+            
+        }
+        else if (attendent.GetBool("IsPushing") == false)
+        {
+            //Stop(stepSound)
+        }
     }
 
     public void Talk() //talking
     {
-        PlaySound(talkSound);
+        if (attendent.GetBool("IsTalking") == true)
+        {
+            PlaySound(talkSound);
+        }
+            
     }
 
     public void Serve() //serving food
     {
-        PlaySound(serveSound);
+        if (attendent.GetBool("IsServing") == true)
+        {
+            PlaySound(serveSound);
+        }
+       
     }
 
     private void PlaySound(EventReference soundEvent) //plays the sound that is meant to be playing at the transform position of the gameobject with this script on it.
     {
         if (soundEvent.IsNull) return; //if there isnt a fmod sound event assigned we skip this so we don't get a gazillion errors.
 
-        RuntimeManager.PlayOneShot(soundEvent, transform.position);
+        //RuntimeManager.PlayOneShot(soundEvent, transform.position);
+        RuntimeManager.PlayOneShot(soundEvent);
     }
 }
