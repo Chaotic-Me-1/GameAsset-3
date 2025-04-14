cbac6735d8819b4bd5324f5f77069b2a8d74af01
OlafRT <mcpikarar@gmail.com>
2025-04-15 01:33:44 +0200

    Added dialogue/gameplay up to the letter in the overhead compartment

--- Assets/scripts/OADS - Olafs Amazing Dialogue System/DialogueTrigger.cs
+++ Assets/scripts/OADS - Olafs Amazing Dialogue System/DialogueTrigger.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public DialogueData[] possibleDialogues;
+    public float triggerDelay = 1f;
 
-    void OnMouseDown()
+    void OnEnable()
     {
+        StartCoroutine(TriggerDialogueAfterDelay());
+    }
+
+    private System.Collections.IEnumerator TriggerDialogueAfterDelay()
+    {
+        yield return new WaitForSeconds(triggerDelay);
+
         int currentLoop = LoopCycleManager.instance != null ? LoopCycleManager.instance.loopCount : 0;
 
-        // Find a dialogue that matches this loop (or is -1 for "any")
         foreach (var d in possibleDialogues)
         {
             if (d != null && (d.requiredLoop == -1 || d.requiredLoop == currentLoop))
             {
                 DialogueManager.instance.StartDialogue(d);
-                return;
+                yield break;
             }
         }
 
